800c85dcdaa7d3510a0e5e7bdde0cdb4dd452272
KH-Tan <tankonghwi@gmail.com>
2024-05-01 15:12:33 +0800

    Initial Commit

---
+++ myAdvanceUI/myAdvanceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAdvanceUIApp.swift
+//  myAdvanceUI
+//
+//  Created by KH Tan on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct myAdvanceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
